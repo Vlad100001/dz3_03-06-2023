90c448d3d686e165d5b2914bea98f62c6ce6ed74
Vlad100001 <132137476+Vlad100001@users.noreply.github.com>
2023-06-03 01:01:55 +0300

    Add files via upload

---
+++ ex23/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+n = Math.Abs(n); // Модуль числа
+for (int i = 1; i <= n; i++)
+    Console.Write($"{i * i *i} ");
