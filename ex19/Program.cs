5b0e43e551b4cc47348e739a3bc4b9b7e7e85306
prudenkov <vladoskiv@yandex.ru>
2023-06-03 00:59:29 +0300

    1

---
+++ ex19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string number = Console.ReadLine()!;
+int len=number.Length;
+if (len==5)
+{
+    if( number[0]==number[4]&& number[1]==number[3])
+    {
+    Console.WriteLine("да");
+    }
+    else     
+    {
+    Console.WriteLine("нет");
+    }
+}
+else
+    {
+    Console.WriteLine("число не 5-тизначное");
+    }
